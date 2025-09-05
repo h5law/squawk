705a165ddd9cb3ca5b3e4e4eea0a249a1de5d971
Vikleo <vikleoboy@gmail.com>
2025-09-05 18:51:47 +0530

    implemented a REPL in main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "string.h"
+#include "stdbool.h"
 
 #ifndef NO_DEBUG
 #define DEBUG(...)                                                             \
@@ -13,6 +15,23 @@
 
 int main(int argc, char **argv)
 {
+    char input[50];
+    char loopEndstr[] = "quit";
+    int  endLoop      = false;
+
+    while (!endLoop) {
+        fgets(input, sizeof(input), stdin);
+        input[strlen(input) - 1] = '\0';
+        endLoop = strcmp(input, loopEndstr) == 0 ? true : false;
+        // printf("in side the loop endloop : %d \n", endLoop);
+
+        if (endLoop) {
+            printf("program is gone quit");
+        } else {
+            printf(" %s,\ninputlen : %lu \n", input, strlen(input));
+        }
+    };
+
     for (int i = 0; i < argc; ++i)
         printf("%s\n", argv[i]);
     return 0;
